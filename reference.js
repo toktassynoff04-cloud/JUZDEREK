f4633b4427287f7eb47a64267d93035f033a5677
toktassynoff04-cloud <toktassynoff04@gmail.com>
2026-08-18 22:33:11 +0500

    Use new mascot only in ancient hero

--- reference.js
+++ reference.js
@@ -1,12 +1,13 @@
 (() => {
-  const primaryMascot = './assets/image-Photoroom%20-%202026-08-18T221923.934.png?v=3';
+  const heroMascot = './assets/image-Photoroom%20-%202026-08-18T223037.285.png?v=4';
 
-  document.querySelectorAll('.mascot').forEach(img => {
-    img.decoding = 'async';
-    img.loading = 'eager';
-    img.onerror = null;
-    img.src = primaryMascot;
-  });
+  const heroImg = document.querySelector('.hero-mascot');
+  if (heroImg) {
+    heroImg.decoding = 'async';
+    heroImg.loading = 'eager';
+    heroImg.onerror = null;
+    heroImg.src = heroMascot;
+  }
 
   const hero = document.getElementById('periodHero');
   const title = document.getElementById('heroTitle');
