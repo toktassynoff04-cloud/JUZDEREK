611db833de81279132d571084b6285b671670f04
toktassynoff04-cloud <toktassynoff04@gmail.com>
2026-08-20 00:14:52 +0500

    Add chapter-based topic navigation

--- data/topics-index.js
+++ data/topics-index.js
@@ -3,3 +3,4 @@ window.JUZDEREK_TOPIC_INDEX=Object.freeze({
   'ancient-greece':Object.freeze({id:'ancient-greece',name:'Ежелгі Грекия өркениеті',period:'ancient',periodLabel:'Ежелгі заман',file:'./data/topics/ancient-greece.json',ready:true})
 });
 window.JUZDEREK_TOPIC_ALIASES=Object.freeze(Object.fromEntries(Object.values(window.JUZDEREK_TOPIC_INDEX).map(t=>[t.name,t.id])));
+(()=>{if(document.querySelector('script[data-juz-chapters]'))return;const s=document.createElement('script');s.src='./chapter-navigation.js?v=20260820-chapters-v1';s.dataset.juzChapters='1';document.head.appendChild(s)})();
