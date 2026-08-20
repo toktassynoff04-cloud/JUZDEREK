5522a42ab4e666306c0dfdccb5471e9cb2a0dd5b
toktassynoff04-cloud <toktassynoff04@gmail.com>
2026-08-20 13:43:12 +0500

    Register Arab Caliphate topic

--- data/topics-index.js
+++ data/topics-index.js
@@ -2,7 +2,8 @@ window.JUZDEREK_TOPIC_INDEX=Object.freeze({
   'ancient-persia':Object.freeze({id:'ancient-persia',name:'Ежелгі Парсы мемлекеті',period:'ancient',periodLabel:'Ежелгі заман',file:'./data/topics/ancient-persia.json',ready:true}),
   'ancient-greece':Object.freeze({id:'ancient-greece',name:'Ежелгі Грекия өркениеті',period:'ancient',periodLabel:'Ежелгі заман',file:'./data/topics/ancient-greece.json',ready:true}),
   'byzantine-empire':Object.freeze({id:'byzantine-empire',name:'Византия империясы',period:'medieval',periodLabel:'Орта ғасыр',file:'./data/topics/byzantine-empire.json',ready:true}),
-  'kievan-rus':Object.freeze({id:'kievan-rus',name:'Киев Русінің құрылуы',period:'medieval',periodLabel:'Орта ғасыр',file:'./data/topics/kievan-rus.json',ready:true})
+  'kievan-rus':Object.freeze({id:'kievan-rus',name:'Киев Русінің құрылуы',period:'medieval',periodLabel:'Орта ғасыр',file:'./data/topics/kievan-rus.json',ready:true}),
+  'arab-caliphate':Object.freeze({id:'arab-caliphate',name:'Араб жаулаушылары. Араб халифатының құрылуы мен ыдырауы',period:'medieval',periodLabel:'Орта ғасыр',file:'./data/topics/arab-caliphate.json',ready:true})
 });
 window.JUZDEREK_TOPIC_ALIASES=Object.freeze(Object.fromEntries(Object.values(window.JUZDEREK_TOPIC_INDEX).map(t=>[t.name,t.id])));
 (()=>{if(document.querySelector('script[data-juz-chapters]'))return;const s=document.createElement('script');s.src='./chapter-navigation.js?v=20260820-chapters-v1';s.dataset.juzChapters='1';document.head.appendChild(s)})();
