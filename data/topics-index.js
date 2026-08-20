e1b90b49f9b5f20e99e08cec304e3b55e78788a4
toktassynoff04-cloud <toktassynoff04@gmail.com>
2026-08-20 14:04:58 +0500

    Register Origin of Islam topic

--- data/topics-index.js
+++ data/topics-index.js
@@ -3,7 +3,8 @@ window.JUZDEREK_TOPIC_INDEX=Object.freeze({
   'ancient-greece':Object.freeze({id:'ancient-greece',name:'Ежелгі Грекия өркениеті',period:'ancient',periodLabel:'Ежелгі заман',file:'./data/topics/ancient-greece.json',ready:true}),
   'byzantine-empire':Object.freeze({id:'byzantine-empire',name:'Византия империясы',period:'medieval',periodLabel:'Орта ғасыр',file:'./data/topics/byzantine-empire.json',ready:true}),
   'kievan-rus':Object.freeze({id:'kievan-rus',name:'Киев Русінің құрылуы',period:'medieval',periodLabel:'Орта ғасыр',file:'./data/topics/kievan-rus.json',ready:true}),
-  'arab-caliphate':Object.freeze({id:'arab-caliphate',name:'Араб жаулаушылары. Араб халифатының құрылуы мен ыдырауы',period:'medieval',periodLabel:'Орта ғасыр',file:'./data/topics/arab-caliphate.json',ready:true})
+  'arab-caliphate':Object.freeze({id:'arab-caliphate',name:'Араб жаулаушылары. Араб халифатының құрылуы мен ыдырауы',period:'medieval',periodLabel:'Орта ғасыр',file:'./data/topics/arab-caliphate.json',ready:true}),
+  'origin-of-islam':Object.freeze({id:'origin-of-islam',name:'Ислам дінінің пайда болуы',period:'medieval',periodLabel:'Орта ғасыр',file:'./data/topics/origin-of-islam.json',ready:true})
 });
 window.JUZDEREK_TOPIC_ALIASES=Object.freeze(Object.fromEntries(Object.values(window.JUZDEREK_TOPIC_INDEX).map(t=>[t.name,t.id])));
 (()=>{if(document.querySelector('script[data-juz-chapters]'))return;const s=document.createElement('script');s.src='./chapter-navigation.js?v=20260820-chapters-v1';s.dataset.juzChapters='1';document.head.appendChild(s)})();
